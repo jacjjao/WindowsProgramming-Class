7d01d87d9b17c55f527544fe1f05082e4b71e7eb
邦甫 蕭 <hjacob1821@gmail.com>
2023-09-14 20:05:01 +0800

    first commit

---
+++ HW1/Calculator.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    class Calculator
+    {
+        private double last_number_ = 0;
+        private char last_op_ = '\0';
+
+        public string calculate(string math)
+        {
+            bool input_invalid = String.IsNullOrEmpty(math) || !Char.IsDigit(math[math.Length - 1]);
+            if (input_invalid)
+            {
+                return null;
+            }
+
+            if (noOp(math))
+            {
+                return chainCalculate(math);
+            }
+            string expr = toPostfixExpr(math);
+            return computeExpr(expr).ToString();
+        }
+
+        private string toPostfixExpr(string math)
+        {
+            string expr = "";
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in math)
+            {
+                if (isOperator(c))
+                {
+                    while (stack.Count > 0 && operatorPriority(stack.Peek()) >= operatorPriority(c))
+                    {
+                        expr += ' ';
+                        expr += stack.Pop();
+                    }
+                    expr += ' ';
+                    stack.Push(c);
+                }
+                else
+                {
+                    expr += c;
+                }
+            }
+
+            while (stack.Count > 0)
+            {
+                expr += ' ';
+                expr += stack.Pop();
+            }
+
+            return expr;
+        }
+
+        private string chainCalculate(string num)
+        {
+            double a = num.Length == 0 ? 0 : double.Parse(num);
+            return compute(a, last_number_, last_op_).ToString();
+        }
+
+        private bool noOp(string expr)
+        {
+            foreach (char c in expr)
+            {
+                if (isOperator(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private double computeExpr(string postfix_expr)
+        {
+            double a, b;
+            Stack<double> stack = new Stack<double>();
+            string[] tokens = postfix_expr.Split(null);
+
+            foreach (string token in tokens)
+            {
+                if (token.Length == 1 && isOperator(token[0]))
+                {
+                    b = last_number_ = stack.Pop();
+                    a = stack.Pop();
+                    stack.Push(compute(a, b, token[0]));
+                    last_op_ = token[0];
+                }
+                else
+                {
+                    stack.Push(double.Parse(token));
+                }
+            }
+
+            return stack.Peek();
+        }
+
+        private double compute(double a, double b, double op)
+        {
+            double result = a;
+
+            switch (op)
+            {
+                case '+':
+                    result += b;
+                    break;
+
+                case '-':
+                    result -= b;
+                    break;
+
+                case '*':
+                    result *= b;
+                    break;
+
+                case '/':
+                    result /= b;
+                    break;
+            }
+
+            return result;
+        }
+
+        private int operatorPriority(char c)
+        {
+            if (c == '*' || c == '/')
+                return 2;
+            else if (c == '+' || c == '-')
+                return 1;
+            return 0;
+        }
+
+        private bool isOperator(char c)
+        {
+            return c == '+' || c == '-' || c == '*' || c == '/';
+        }
+    }
+}
---
+++ HW1/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW1
+{
+    public partial class AppForm : Form
+    {
+        public AppForm()
+        {
+            InitializeComponent();
+            result_ = "";
+            calculator_ = new Calculator();
+            dot_ = false;
+        }
+
+        private Calculator calculator_;
+        private string result_;
+        private bool dot_;
+
+        private void buttonCE_Click(object sender, EventArgs e)
+        {
+            textBox.Text = result_;
+            dot_ = !double.TryParse(result_, out _);
+        }
+
+        private void buttonC_Click(object sender, EventArgs e)
+        {
+            textBox.Text = result_ = "";
+            dot_ = false;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox.Text += '9';
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox.Text += '8';
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox.Text += '7';
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox.Text += '6';
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox.Text += '5';
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox.Text += '4';
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox.Text += '3';
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox.Text += '2';
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox.Text += '1';
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            textBox.Text += '0';
+        }
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(textBox.Text) && Char.IsNumber(textBox.Text[textBox.Text.Length - 1]))
+            {
+                textBox.Text += '+';
+                dot_ = false;
+            }
+        }
+
+        private void buttonMinus_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(textBox.Text) && Char.IsNumber(textBox.Text[textBox.Text.Length - 1]))
+            {
+                textBox.Text += '-';
+                dot_ = false;
+            }
+        }
+
+        private void buttonMul_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(textBox.Text) && Char.IsNumber(textBox.Text[textBox.Text.Length - 1]))
+            {
+                textBox.Text += 'x';
+                dot_ = false;
+            }
+        }
+
+        private void buttonSub_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(textBox.Text) && Char.IsNumber(textBox.Text[textBox.Text.Length - 1]))
+            {
+                textBox.Text += '/';
+                dot_ = false;
+            }
+        }
+
+        private void buttonDot_Click(object sender, EventArgs e)
+        {
+            bool can_insert =
+                !dot_ &&
+                !String.IsNullOrEmpty(textBox.Text) &&
+                 Char.IsNumber(textBox.Text[textBox.Text.Length - 1]);
+            if (can_insert)
+            {
+                textBox.Text += '.';
+                dot_ = true;
+            }
+        }
+
+        private void buttonEqual_Click(object sender, EventArgs e)
+        {
+            string result = calculator_.calculate(textBox.Text.Replace('x', '*'));
+            if (!String.IsNullOrEmpty(result))
+            {
+                result_ = textBox.Text = result;
+            }
+        }
+    }
+}
