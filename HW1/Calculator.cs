54050188ea0bc833992029712f2e71163c49b061
邦甫 蕭 <hjacob1821@gmail.com>
2023-09-15 16:58:14 +0800

    formatting

--- HW1/Calculator.cs
+++ HW1/Calculator.cs
@@ -1,40 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace HW1
-{
-    class Calculator
-    {
+namespace HW1 {
+    class Calculator {
         private double _lastNumber;
         private char _lastOp;
-        private readonly Dictionary<char, int> _operatorDict = new Dictionary<char, int> 
-        {
+        private readonly Dictionary<char, int> _operatorDict = new Dictionary<char, int> {
             { '+', 1 },
             { '-', 1 },
             { 'x', 2 },
-            { '/', 2 } 
+            { '/', 2 }
         };
 
-        public Calculator()
-        {
+        public Calculator() {
             _lastOp = '\0';
             _lastNumber = 0;
         }
 
         /* 計算textbox上的數學式並回傳結果 */
-        public string Calculate(string mathExpr)
-        {
+        public string Calculate(string mathExpr) {
             bool inputInvalid = String.IsNullOrEmpty(mathExpr) || !Char.IsDigit(mathExpr[mathExpr.Length - 1]);
-            if (inputInvalid)
-            {
+            if (inputInvalid) {
                 return null;
             }
 
-            if (!mathExpr.Any(c => IsOperator(c)))
-            {
+            if (!mathExpr.Any(c => IsOperator(c))) {
                 return ChainCalculate(mathExpr);
             }
             string postfixExpr = ToPostfixExpr(mathExpr);
@@ -42,31 +33,25 @@ namespace HW1
         }
 
         /* 將中序數學式轉成後序 ex. (1 + 2 * 3) 轉換後會變成 (1 2 3 * +) */
-        private string ToPostfixExpr(string mathExpr)
-        {
+        private string ToPostfixExpr(string mathExpr) {
             string expr = "";
             Stack<char> stack = new Stack<char>();
 
-            foreach (char c in mathExpr)
-            {
-                if (IsOperator(c))
-                {
-                    while (stack.Count > 0 && OperatorPriority(stack.Peek()) >= OperatorPriority(c))
-                    {
+            foreach (char c in mathExpr) {
+                if (IsOperator(c)) {
+                    while (stack.Count > 0 && OperatorPriority(stack.Peek()) >= OperatorPriority(c)) {
                         expr += ' ';
                         expr += stack.Pop();
                     }
                     expr += ' ';
                     stack.Push(c);
                 }
-                else
-                {
+                else {
                     expr += c;
                 }
             }
 
-            while (stack.Count > 0)
-            {
+            while (stack.Count > 0) {
                 expr += ' ';
                 expr += stack.Pop();
             }
@@ -75,30 +60,25 @@ namespace HW1
         }
 
         /* 實現計算機可以連續計算的功能 */
-        private string ChainCalculate(string num)
-        {
+        private string ChainCalculate(string num) {
             double a = (num.Length == 0) ? 0 : Double.Parse(num, System.Globalization.NumberStyles.Float);
             return Compute(a, _lastNumber, _lastOp).ToString();
         }
 
         /* 計算後序數學式並將結果傳回 */
-        private double EvaluatePostfixMathExpr(string postfixExpr)
-        {
+        private double EvaluatePostfixMathExpr(string postfixExpr) {
             double a, b;
             Stack<double> stack = new Stack<double>();
             string[] tokens = postfixExpr.Split(null);
 
-            foreach (string token in tokens)
-            {
-                if (token.Length == 1 && IsOperator(token[0]))
-                {
+            foreach (string token in tokens) {
+                if (token.Length == 1 && IsOperator(token[0])) {
                     b = _lastNumber = stack.Pop();
                     a = stack.Pop();
                     stack.Push(Compute(a, b, token[0]));
                     _lastOp = token[0];
                 }
-                else
-                {
+                else {
                     stack.Push(Double.Parse(token, System.Globalization.NumberStyles.Float));
                 }
             }
@@ -107,12 +87,10 @@ namespace HW1
         }
 
         /* 根據傳入的op對a b做四則運算 */
-        private double Compute(double a, double b, char op)
-        {
+        private double Compute(double a, double b, char op) {
             double result = a;
 
-            switch (op)
-            {
+            switch (op) {
                 case '+':
                     result += b;
                     break;
@@ -134,21 +112,18 @@ namespace HW1
             }
 
             return result;
-        } 
+        }
 
         /* implement中序轉後序的演算法時需要用到的function */
-        private int OperatorPriority(char c)
-        {
-            if (IsOperator(c))
-            {
+        private int OperatorPriority(char c) {
+            if (IsOperator(c)) {
                 return _operatorDict[c];
             }
             return 0;
         }
 
         /* 檢查c是不是四則運算的符號 */
-        private bool IsOperator(char c)
-        {
+        private bool IsOperator(char c) {
             return _operatorDict.ContainsKey(c);
         }
     }
--- HW1/Form1.cs
+++ HW1/Form1.cs
@@ -1,19 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace HW1
-{
-    public partial class AppForm : Form
-    {
-        public AppForm()
-        {
+namespace HW1 {
+    public partial class AppForm : Form {
+        public AppForm() {
             InitializeComponent();
             _result = "";
             _calculator = new Calculator();
@@ -25,139 +15,115 @@ namespace HW1
         private bool _dot;
 
         /* 處理button CE被點擊時的event */
-        private void ButtonCEClick(object sender, EventArgs e)
-        {
+        private void ButtonCEClick(object sender, EventArgs e) {
             textBox.Text = _result;
             _dot = !double.TryParse(_result, out _);
         }
 
         /* 處理button C被點擊時的event */
-        private void ButtonCClick(object sender, EventArgs e)
-        {
+        private void ButtonCClick(object sender, EventArgs e) {
             textBox.Text = _result = "";
             _dot = false;
         }
 
         /* 處理button 9被點擊時的event */
-        private void Button9Click(object sender, EventArgs e)
-        {
+        private void Button9Click(object sender, EventArgs e) {
             textBox.Text += button9.Text;
         }
 
         /* 處理button 8被點擊時的event */
-        private void Button8Click(object sender, EventArgs e)
-        {
+        private void Button8Click(object sender, EventArgs e) {
             textBox.Text += button8.Text;
         }
 
         /* 處理button 7被點擊時的event */
-        private void Button7Click(object sender, EventArgs e)
-        {
+        private void Button7Click(object sender, EventArgs e) {
             textBox.Text += button7.Text;
         }
 
         /* 處理button 6被點擊時的event */
-        private void Button6Click(object sender, EventArgs e)
-        {
+        private void Button6Click(object sender, EventArgs e) {
             textBox.Text += button6.Text;
         }
 
         /* 處理button 5被點擊時的event */
-        private void Button5Click(object sender, EventArgs e)
-        {
+        private void Button5Click(object sender, EventArgs e) {
             textBox.Text += button5.Text;
         }
 
         /* 處理button 4被點擊時的event */
-        private void Button4Click(object sender, EventArgs e)
-        {
+        private void Button4Click(object sender, EventArgs e) {
             textBox.Text += button4.Text;
         }
 
         /* 處理button 3被點擊時的event */
-        private void Button3Click(object sender, EventArgs e)
-        {
+        private void Button3Click(object sender, EventArgs e) {
             textBox.Text += button3.Text;
         }
 
         /* 處理button 2被點擊時的event */
-        private void Button2Click(object sender, EventArgs e)
-        {
+        private void Button2Click(object sender, EventArgs e) {
             textBox.Text += button2.Text;
         }
 
         /* 處理button 1被點擊時的event */
-        private void Button1Click(object sender, EventArgs e)
-        {
+        private void Button1Click(object sender, EventArgs e) {
             textBox.Text += button1.Text;
         }
 
         /* 處理button 0被點擊時的event */
-        private void Button0Click(object sender, EventArgs e)
-        {
+        private void Button0Click(object sender, EventArgs e) {
             textBox.Text += button0.Text;
         }
 
         /* 處理button plus被點擊時的event */
-        private void ButtonPlusClick(object sender, EventArgs e)
-        {
-            if (!String.IsNullOrEmpty(textBox.Text) && Char.IsNumber(textBox.Text[textBox.Text.Length - 1]))
-            {
+        private void ButtonPlusClick(object sender, EventArgs e) {
+            if (!String.IsNullOrEmpty(textBox.Text) && Char.IsNumber(textBox.Text[textBox.Text.Length - 1])) {
                 textBox.Text += buttonPlus.Text;
                 _dot = false;
             }
         }
 
         /* 處理button minus被點擊時的event */
-        private void ButtonSubClick(object sender, EventArgs e)
-        {
-            if (!String.IsNullOrEmpty(textBox.Text) && Char.IsNumber(textBox.Text[textBox.Text.Length - 1]))
-            {
+        private void ButtonSubClick(object sender, EventArgs e) {
+            if (!String.IsNullOrEmpty(textBox.Text) && Char.IsNumber(textBox.Text[textBox.Text.Length - 1])) {
                 textBox.Text += buttonSub.Text;
                 _dot = false;
             }
         }
 
         /* 處理button multiply被點擊時的event */
-        private void ButtonMulClick(object sender, EventArgs e)
-        {
-            if (!String.IsNullOrEmpty(textBox.Text) && Char.IsNumber(textBox.Text[textBox.Text.Length - 1]))
-            {
+        private void ButtonMulClick(object sender, EventArgs e) {
+            if (!String.IsNullOrEmpty(textBox.Text) && Char.IsNumber(textBox.Text[textBox.Text.Length - 1])) {
                 textBox.Text += buttonMul.Text;
                 _dot = false;
             }
         }
 
         /* 處理button divide被點擊時的event */
-        private void ButtonDivClick(object sender, EventArgs e)
-        {
-            if (!String.IsNullOrEmpty(textBox.Text) && Char.IsNumber(textBox.Text[textBox.Text.Length - 1]))
-            {
+        private void ButtonDivClick(object sender, EventArgs e) {
+            if (!String.IsNullOrEmpty(textBox.Text) && Char.IsNumber(textBox.Text[textBox.Text.Length - 1])) {
                 textBox.Text += buttonDiv.Text;
                 _dot = false;
             }
         }
 
         /* 處理button dot被點擊時的event */
-        private void ButtonDotClick(object sender, EventArgs e)
-        {
+        private void ButtonDotClick(object sender, EventArgs e) {
             bool canInsert =
                 !_dot &&
                 !String.IsNullOrEmpty(textBox.Text) &&
                  Char.IsNumber(textBox.Text[textBox.Text.Length - 1]);
-            if (canInsert)
-            {
+            if (canInsert) {
                 textBox.Text += buttonDot.Text;
                 _dot = true;
             }
         }
 
         /* 處理button equal被點擊時的event */
-        private void ButtonEqualClick(object sender, EventArgs e)
-        {
+        private void ButtonEqualClick(object sender, EventArgs e) {
             string result = _calculator.Calculate(textBox.Text);
-            if (!String.IsNullOrEmpty(result))
-            {
+            if (!String.IsNullOrEmpty(result)) {
                 _result = textBox.Text = result;
             }
         }
